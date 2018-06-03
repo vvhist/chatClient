7409b7a091c7facc97e42552bcaa77fa28c7f482
unknown <vvhist@gmail.com>
2018-06-03 22:04:48 +0300

    Login window with password verification

--- src/java/client/Presenter.java
+++ src/java/client/Presenter.java
@@ -1,61 +1,97 @@
 package client;
 
 import javax.swing.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+
+import java.awt.event.*;
 import java.io.*;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class Presenter {
+public final class Presenter {
 
-    private SwingView view = new SwingView();
+    private Login loginForm;
+    private Chat chatForm;
+
     private Map<String, JTextArea> dialogs = new HashMap<>();
-    private PrintWriter output;
+    private PrintWriter output = null;
 
     public Presenter() {
         new ConnectionHandler().execute();
-        addNewContact("Server");
-        view.displayStatus("Please log in");
+        loginForm = new Login(new CloseOperation());
 
-        view.getLoginButton().addActionListener(e -> {
-            String name = view.getNameTextField().getText();
-
-            if (name.isEmpty() || name.equals("Server") || name.contains(":")
-                                                        || name.contains(" ")) {
-                return;
-            }
-            output.println("name " + name);
-            view.displayStatus(name + " is online");
+        loginForm.getLoginButton().addActionListener(e -> {
+            String authData = getAuthData();
+            if (authData.isEmpty()) return;
+            output.println("/log/" + authData);
         });
 
-        view.getSendButton().addActionListener(e -> {
-            String outputText = view.getOutputTextField().getText();
+        loginForm.getRegisterButton().addActionListener(e -> {
+            String authData = getAuthData();
+            if (authData.isEmpty()) return;
+            output.println("/reg/" + authData);
+        });
+    }
+
+    private String getAuthData() {
+        String username = loginForm.getUsernameField().getText();
+        char[] password = loginForm.getPasswordField().getPassword();
+
+        if (username.isEmpty() || password.length == 0) return "";
+        if (username.contains("/") || username.contains(":") || username.contains(" ")) {
+            loginForm.displayWarning("A username cannot contain /, : or spaces");
+            return "";
+        }
+        StringBuilder builder = new StringBuilder(username + "/");
+        for (char c : password) {
+            builder.append(c);
+        }
+        Arrays.fill(password, '0');
+        return builder.toString();
+    }
+
+    private void addChatListeners() {
+        chatForm.getSendButton().addActionListener(e -> {
+            String outputText = chatForm.getOutputTextField().getText();
             if (outputText.isEmpty()) return;
 
-            String contact = view.getTabbedPane().getTitleAt(
-                             view.getTabbedPane().getSelectedIndex());
+            String contact = chatForm.getTabbedPane().getTitleAt(
+                    chatForm.getTabbedPane().getSelectedIndex());
             output.println(contact + " " + outputText);
-            view.getOutputTextField().setText("");
+            chatForm.getOutputTextField().setText("");
         });
 
-        view.getOutputTextField().addKeyListener(new KeyAdapter() {
+        chatForm.getOutputTextField().addKeyListener(new KeyAdapter() {
+
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    view.getSendButton().doClick();
+                    chatForm.getSendButton().doClick();
                 }
             }
         });
     }
 
     private void addNewContact(String name) {
-        JTextArea textArea = view.addTab(name);
+        JTextArea textArea = chatForm.addTab(name);
         dialogs.put(name, textArea);
     }
+
+
+    private final class CloseOperation extends WindowAdapter {
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+            if (output != null) {
+                output.println("/exit");
+            }
+            System.exit(0);
+        }
+    }
+
 
     private class ConnectionHandler extends SwingWorker<Void, String> {
 
@@ -82,8 +118,18 @@ public class Presenter {
         @Override
         protected void process(List<String> inputs) {
             for (String input : inputs) {
-                if (input.startsWith("newChat ")) {
-                    String contact = input.replace("newChat ", "");
+                if (input.equals("/pass")) {
+                    loginForm.close();
+                    loginForm = null;
+                    chatForm = new Chat(new CloseOperation());
+                    addChatListeners();
+                    addNewContact("Server");
+                } else if (input.equals("/deny")) {
+                    loginForm.displayWarning("Incorrect username or password");
+                } else if (input.equals("/denyName")) {
+                    loginForm.displayWarning("This name is already taken");
+                } else if (input.startsWith("/newDialog ")) {
+                    String contact = input.replace("/newDialog ", "");
                     if (!dialogs.containsKey(contact)) {
                         addNewContact(contact);
                     } else {
@@ -106,7 +152,13 @@ public class Presenter {
             try {
                 get();
             } catch (InterruptedException | ExecutionException e) {
-                view.displayStatus(e.getMessage());
+                String warning = "Failed to connect to server";
+                if (loginForm != null) {
+                    loginForm.enableComponents(loginForm.$$$getRootComponent$$$(), false);
+                    loginForm.displayWarning(warning);
+                } else if (chatForm != null) {
+                    chatForm.displayStatus(warning);
+                }
                 e.printStackTrace();
             }
         }
